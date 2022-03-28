26c06ee34444799fdc61104f85a06b61e63d30b3
Atthiyya <atthiyya@gmail.com>
2022-03-29 00:00:42 +0700

    Add Code Room and several lighting

--- Assets/UsernameDisplay.cs
+++ Assets/UsernameDisplay.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
+
 public class UsernameDisplay : MonoBehaviour
 {
     [SerializeField] PhotonView _photonView;
     [SerializeField] TMP_Text nameText;
+    [SerializeField] TMP_Text roomText;
+    [SerializeField] TMP_Text currentUsers;
+
     void Start()
     {
         if (_photonView.IsMine) {
@@ -14,6 +19,13 @@ public class UsernameDisplay : MonoBehaviour
         }
 
         nameText.text = _photonView.Owner.NickName;
+
+        if (_photonView.IsMine)
+        {
+            gameObject.SetActive(true);
+        }
+        roomText.text = "Room Code: " + PhotonNetwork.CurrentRoom.Name;
+        currentUsers.text = "Currently users in this room:" + PhotonNetwork.PlayerList;
     }
     
 }
