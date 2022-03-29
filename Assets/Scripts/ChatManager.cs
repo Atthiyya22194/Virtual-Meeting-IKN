71c9e327771bd3c636701010b88c24bb1e6cff4d
jonathan <jonathan.franzeli@student.umn.ac.id>
2022-03-29 13:31:55 +0700

    added working text chat

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -13,4 +13,70 @@ public class ChatManager : MonoBehaviour
     private List<string> messages = new List<string>();
     private float buildDelay = 0f;
     private int maximumMessages = 14;
+
+    public TMP_InputField chatInput;
+    public TextMeshProUGUI chatContent;
+
+    void Start() {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    void Update() {
+        if (PhotonNetwork.InRoom) {
+            chatContent.maxVisibleLines = maximumMessages;
+            if (messages.Count > maximumMessages) {
+                messages.RemoveAt(0);
+            }
+            if (buildDelay < Time.time) {
+                BuildChatContents();
+                buildDelay = Time.time + 0.25f;
+            }
+        } else if (messages.Count > 0) {
+            messages.Clear();
+            chatContent.text = "";
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return)) {
+            if (EventSystem.current.currentSelectedGameObject == chatInput) {
+                //Debug.Log("Test 1");
+                EventSystem.current.SetSelectedGameObject(chatInput.gameObject, null);
+                chatInput.OnPointerClick(new PointerEventData(EventSystem.current));
+            } else {
+                //Debug.Log("Test 2");
+                SubmitChat();
+            }
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddNewMessage(string msg) {
+        messages.Add(msg);
+    }
+
+    public void SendChat(string msg) {
+        string newMessage = PhotonNetwork.NickName + ": " + msg;
+        photonView.RPC("RPC_AddNewMessage", RpcTarget.All, newMessage);
+    }
+
+    public void SubmitChat() {
+        string blankCheck = chatInput.text;
+        blankCheck = Regex.Replace(blankCheck, @"\s", "");
+        if (blankCheck == "") {
+            chatInput.DeactivateInputField();
+            chatInput.text = "";
+            return;
+        }
+
+        SendChat(chatInput.text);
+        chatInput.DeactivateInputField();
+        chatInput.text = "";
+    }
+
+    void BuildChatContents() {
+        string newContents = "";
+        foreach (string s in messages) {
+            newContents += s + "\n";
+        }
+        chatContent.text = newContents;
+    }
 }
