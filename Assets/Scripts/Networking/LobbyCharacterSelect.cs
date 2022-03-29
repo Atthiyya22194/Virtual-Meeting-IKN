686301b027c1eed7a264d4acfc2041dfb8d44176
ironyse <luk3man.lh@gmail.com>
2022-03-30 00:01:03 +0700

    adding new character select

---
+++ Assets/Scripts/Networking/LobbyCharacterSelect.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyCharacterSelect : MonoBehaviourPunCallbacks
+{
+    ExitGames.Client.Photon.Hashtable playerProperties = new ExitGames.Client.Photon.Hashtable();
+
+    [SerializeField] GameObject charSelectCanvas;
+    [SerializeField] RawImage playerAvatar;
+    [SerializeField] RenderTexture[] renderTextureAvatars;
+
+    private void Start() {
+        UpdatePlayerAvatar();
+    }
+
+    public void OnCharSelect() {
+        charSelectCanvas.SetActive(true);
+    }
+    public void OnPrevButton_Clicked() {
+        if ((int)playerProperties["playerAvatar"] == 0) {
+            playerProperties["playerAvatar"] = renderTextureAvatars.Length - 1;
+        }
+        else {
+            playerProperties["playerAvatar"] = (int)playerProperties["playerAvatar"] - 1;
+        }
+        
+        PhotonNetwork.SetPlayerCustomProperties(playerProperties);        
+    }
+
+    public void OnNextButton_Clicked() {
+        if ((int)playerProperties["playerAvatar"] == renderTextureAvatars.Length - 1) {
+            playerProperties["playerAvatar"] = 0;
+        }
+        else {
+            playerProperties["playerAvatar"] = (int)playerProperties["playerAvatar"] + 1;
+        }        
+        
+        PhotonNetwork.SetPlayerCustomProperties(playerProperties);
+    }
+
+    public void OnMasukButton_Clicked() {
+        // Send messages to listener
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps) {
+        if (targetPlayer == PhotonNetwork.LocalPlayer) {
+            UpdatePlayerAvatar(); 
+        }
+    }
+
+    public void UpdatePlayerAvatar() {        
+        playerAvatar.texture = renderTextureAvatars[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
+    }
+}
