f528445d596a59e6a772f0c186f1f9171b57d752
ironyse <luk3man.lh@gmail.com>
2022-04-04 17:52:27 +0700

    fixing cursor state

--- Assets/CursorStateChanger.cs
+++ Assets/CursorStateChanger.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CursorStateChanger : MonoBehaviour
+public class CursorStateChanger : MonoBehaviour, IPointerDownHandler
 {
+    GraphicRaycaster raycaster;
+
+    private void Start() {
+        raycaster = GetComponent<GraphicRaycaster>();
+    }
+
     private void Update() {
-        if (Keyboard.current.escapeKey.wasReleasedThisFrame && Cursor.lockState == CursorLockMode.Locked) {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
     }
-    public void LockCursor() {        
-        Cursor.lockState = CursorLockMode.Locked;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        raycaster.Raycast(eventData, results);
+
+        foreach (RaycastResult result in results) {
+            if (result.gameObject == gameObject) {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
     }
 
 }
