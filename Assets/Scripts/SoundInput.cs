1996dd18dcf5d571d5aa07c54239ad59502757bd
jonathan <jonathan.franzeli@student.umn.ac.id>
2022-04-07 10:52:16 +0700

    fixed voice chat in webgl

--- Assets/Scripts/SoundInput.cs
+++ Assets/Scripts/SoundInput.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using FrostweepGames.VoicePro;
+using FrostweepGames.Plugins.Native;
 
 public class SoundInput : MonoBehaviour
 {
@@ -10,33 +12,74 @@ public class SoundInput : MonoBehaviour
     public bool isRecording;
     public bool buttonToggle;
 
+    public Button muteMyClientButton;
+    private bool muteMyClientToggle;
+
     void Awake() {
-        buttonToggle = false;
+        // buttonToggle = false;
+        muteMyClientToggle = false;
+    }
+
+    void Start() {
+        UpdateMicrophone();
+        muteMyClientButton.onClick.AddListener(MuteMyClientButtonClicked);
     }
 
     void Update() {
-        if (buttonToggle == true && !isRecording) {
-            StartRecord();
-        } else if (buttonToggle == false && isRecording) {
-            StopRecord();
+        // if (buttonToggle == true && !isRecording) {
+        //     StartRecord();
+        // } else if (buttonToggle == false && isRecording) {
+        //     StopRecord();
+        // }
+    }
+
+    // public void Record() {
+    //     if (Input.GetKeyDown(KeyCode.R) && !isRecording) {
+    //         StartRecord();
+    //     } else if (Input.GetKeyUp(KeyCode.R) && isRecording) {
+    //         StopRecord();
+    //     }
+    // }
+
+    // public void StartRecord() {
+    //     recorder.RefreshMicrophones();
+    //     Debug.Log("Refreshed microphone list");
+    //     recorder.StartRecord();
+    //     isRecording = true;
+    //     Debug.Log("Is currently recording");
+    // }
+
+    // public void StopRecord() {
+    //     recorder.StopRecord();
+    //     isRecording = false;
+    //     Debug.Log("Recording stopped");
+    // }
+
+    private void MuteMyClientButtonClicked() {
+        muteMyClientToggle = !muteMyClientToggle;
+        if (muteMyClientToggle) {
+            UpdateMicrophone();
+            if (!NetworkRouter.Instance.ReadyToTransmit || !recorder.StartRecord()) {
+                //muteMyClientToggle.isOn = false;
+                recorder.StartRecord();
+                //Debug.Log("True 1");
+            }
+            Debug.Log("True 2");
+        } else {
+            recorder.StopRecord();
+            //Debug.Log("recorder.StopRecord();");
+            //Debug.Log("False");
         }
     }
 
-    public void Record() {
-        if (Input.GetKeyDown(KeyCode.R) && !isRecording) {
-            StartRecord();
-        } else if (Input.GetKeyUp(KeyCode.R) && isRecording) {
-            StopRecord();
+    public void UpdateMicrophone() {
+        recorder.RefreshMicrophones();
+        Debug.Log("Refreshed mics");
+        foreach (var device in CustomMicrophone.devices)
+        {
+            Debug.Log("Name: " + device);
         }
-    }
-
-    public void StartRecord() {
-        recorder.StartRecord();
-        isRecording = true;
-    }
-
-    public void StopRecord() {
-        recorder.StopRecord();
-        isRecording = false;
+        recorder.SetMicrophone(CustomMicrophone.devices[0]);
+        Debug.Log("Active mic: " + CustomMicrophone.devices[0]);
     }
 }
